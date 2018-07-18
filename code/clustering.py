007d01a59c0620be4e2eb122a32f8a17d7c6e5b5
a526547870 <liuzhihan027@163.com>
2018-07-18 17:46:48 +0800

    聚类分析_层次聚类部分

--- code/clustering.py
+++ code/clustering.py
@@ -6,20 +6,20 @@ import math, random
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 
+
 # k_means主类
 class KMeans:
     """performs k-means clustering"""
 
     # 初始化参数
     def __init__(self, k):
-        self.k = k          # number of clusters(聚类中心个数)
-        self.means = None   # means of clusters(聚类中心)
+        self.k = k  # number of clusters(聚类中心个数)
+        self.means = None  # means of clusters(聚类中心)
 
     # 分类(返回单个点到每一个中心的距离,取距离最近的点即中心点返回中心点的索引)
     def classify(self, input):
         """return the index of the cluster closest to the input"""
         # min(range(self.k)--定位后面的i的取值,key=lambda i: squared_distance(input, self.means[i]))--对每个key取最小值
-
 
         return min(range(self.k),
                    key=lambda i: squared_distance(input, self.means[i]))
@@ -31,18 +31,17 @@ class KMeans:
         self.means = random.sample(inputs, self.k)
         assignments = None
 
-        
         while True:
             # Find new assignments
             # 所有点对应的索引集合
             new_assignments = map(self.classify, inputs)
-
             # If no assignments have changed, we're done.
-            if assignments == new_assignments:                
+
+            if assignments == new_assignments:
                 return
 
             # Otherwise keep the new assignments,
-            assignments = new_assignments    
+            assignments = new_assignments
 
             for i in range(self.k):
                 i_points = [p for p, a in zip(inputs, assignments) if a == i]
@@ -50,22 +49,24 @@ class KMeans:
                 # 防止空串,更新第i个中心点
                 if i_points:
                     # 属性内的全部点的矢量和的均值来更新中心点
-                    self.means[i] = vector_mean(i_points)    
+                    self.means[i] = vector_mean(i_points)
 
-# 全部点距离中心点的距离的和(整体误差)
+                # 全部点距离中心点的距离的和(整体误差)
+
+# 误差的平方和
 def squared_clustering_errors(inputs, k):
     """finds the total squared error from k-means clustering the inputs"""
     clusterer = KMeans(k)
     clusterer.train(inputs)
     means = clusterer.means
     assignments = map(clusterer.classify, inputs)
-    
-    return sum(squared_distance(input,means[cluster])
+
+    return sum(squared_distance(input, means[cluster])
                for input, cluster in zip(inputs, assignments))
+
 
 # 画出不同k值得到的聚类中心的误差的趋势
 def plot_squared_clustering_errors(plt):
-
     ks = range(1, len(inputs) + 1)
     errors = [squared_clustering_errors(inputs, k) for k in ks]
 
@@ -75,38 +76,48 @@ def plot_squared_clustering_errors(plt):
     plt.ylabel("total squared error")
     plt.show()
 
+
 #
 # using clustering to recolor an image
 # 用聚类法重建图像
 #
 
 def recolor_image(input_file, k=5):
-
-    img = mpimg.imread(path_to_png_file)
+    # 由路径获取文件
+    img = mpimg.imread(input_file)
+    # 将文件的数组扁平化
     pixels = [pixel for row in img for pixel in row]
+    # 聚类训练
     clusterer = KMeans(k)
-    clusterer.train(pixels) # this might take a while    
+    print "开始训练数据,较慢~~~~~~~~"
+    clusterer.train(pixels)  # this might take a while
 
+    # 对数据进行分类聚合
     def recolor(pixel):
-        cluster = clusterer.classify(pixel) # index of the closest cluster
-        return clusterer.means[cluster]     # mean of the closest cluster
+        cluster = clusterer.classify(pixel)  # index of the closest cluster
+        return clusterer.means[cluster]  # mean of the closest cluster
 
+    # 对每行数据使用聚类后的聚类中心对其进行分类,使用分类结果
     new_img = [[recolor(pixel) for pixel in row]
                for row in img]
 
+    # 处理后的数据展示
     plt.imshow(new_img)
     plt.axis('off')
     plt.show()
+
 
 #
 # hierarchical clustering
 # 层次聚类
 #
 
+# 判断是否是叶子结点
 def is_leaf(cluster):
     """a cluster is a leaf if it has length 1"""
     return len(cluster) == 1
 
+# 获取孩子结点
 def get_children(cluster):
     """returns the two children of this cluster if it's a merged cluster;
     raises an exception if this is a leaf cluster"""
@@ -115,39 +126,46 @@ def get_children(cluster):
     else:
         return cluster[1]
 
+# 递归得到簇下全部叶子的值
 def get_values(cluster):
     """returns the value in this cluster (if it's a leaf cluster)
     or all the values in the leaf clusters below it (if it's not)"""
     if is_leaf(cluster):
-        return cluster # is already a 1-tuple containing value
+        return cluster  # is already a 1-tuple containing value
     else:
         return [value
                 for child in get_children(cluster)
                 for value in get_values(child)]
 
+
+# 聚类的距离
 def cluster_distance(cluster1, cluster2, distance_agg=min):
     """finds the aggregate distance between elements of cluster1
     and elements of cluster2"""
     return distance_agg([distance(input1, input2)
-                        for input1 in get_values(cluster1)
-                        for input2 in get_values(cluster2)])
+                         for input1 in get_values(cluster1)
+                         for input2 in get_values(cluster2)])
+
 
 def get_merge_order(cluster):
     if is_leaf(cluster):
         return float('inf')
     else:
-        return cluster[0] # merge_order is first element of 2-tuple
+        return cluster[0]  # merge_order is first element of 2-tuple
 
+# 自底向上聚类主函数
 def bottom_up_cluster(inputs, distance_agg=min):
     # start with every input a leaf cluster / 1-tuple
+    # 开始使每个输入为一类
     clusters = [(input,) for input in inputs]
-    
+
     # as long as we have more than one cluster left...
     while len(clusters) > 1:
         # find the two closest clusters
+        # 找到两个最近的簇
         c1, c2 = min([(cluster1, cluster2)
-                     for i, cluster1 in enumerate(clusters)
-                     for cluster2 in clusters[:i]],
+                      for i, cluster1 in enumerate(clusters)
+                      for cluster2 in clusters[:i]],
                      key=lambda (x, y): cluster_distance(x, y, distance_agg))
 
         # remove them from the list of clusters
@@ -162,10 +180,11 @@ def bottom_up_cluster(inputs, distance_agg=min):
     # when there's only one cluster left, return it
     return clusters[0]
 
+
 def generate_clusters(base_cluster, num_clusters):
     # start with a list with just the base cluster
     clusters = [base_cluster]
-    
+
     # as long as we don't have enough clusters yet...
     while len(clusters) < num_clusters:
         # choose the last-merged of our clusters
@@ -178,11 +197,14 @@ def generate_clusters(base_cluster, num_clusters):
     # once we have enough clusters...
     return clusters
 
+
 if __name__ == "__main__":
 
-    inputs = [[-14,-5],[13,13],[20,23],[-19,-11],[-9,-16],[21,27],[-49,15],[26,13],[-46,5],[-34,-1],[11,15],[-49,0],[-22,-16],[19,28],[-12,-8],[-13,-19],[-41,8],[-11,-6],[-25,-9],[-18,-3]]
+    inputs = [[-14, -5], [13, 13], [20, 23], [-19, -11], [-9, -16], [21, 27], [-49, 15], [26, 13], [-46, 5], [-34, -1],
+              [11, 15], [-49, 0], [-22, -16], [19, 28], [-12, -8], [-13, -19], [-41, 8], [-11, -6], [-25, -9],
+              [-18, -3]]
 
-    random.seed(0) # so you get the same results as me
+    random.seed(0)  # so you get the same results as me
     clusterer = KMeans(3)
     clusterer.train(inputs)
     print "3-means:"
@@ -206,50 +228,22 @@ if __name__ == "__main__":
     # 画出不同k值聚类的总误差趋势
     plot_squared_clustering_errors(plt)
 
-    # 图像聚类
-    # 将路径下图像装载成数组
-    # img[i][j]表示第i行第j列的像素
-    path_to_png_file = r"image.png"
-    import matplotlib.image as mping
-    img = mping.imread( path_to_png_file )
-
-    top_row = img[0]
-    top_left_pixel = top_row[0]
-    print top_left_pixel
-    red,green,blue,aa = top_left_pixel
-
-    # 将像素数据扁平化
-    pixels = [pixel for row in img for pixel in row]
-    clusterer = KMeans(5) # 设定参数
-    clusterer.train(pixels) # 迭代寻找聚类中心(很慢!很慢!很慢!)
-
-    # 使用聚类中心对数据进行分类
-    def recolor(pixel):
-        cluster = clusterer.classify(pixel)
-        return clusterer.means[cluster]
-
-    # 按行进行处理
-    new_img = [[recolor(pixel) for pixel in row] for row in img]
-
-    plt.imshow(new_img)
-    plt.axis('off')
-    plt.show()
-
-
+    # 图像聚类(较慢)
+    # recolor_image(r"image.png", k=5)
 
     print "bottom up hierarchical clustering"
     # 自下向上层次聚类
 
     base_cluster = bottom_up_cluster(inputs)
     print base_cluster
-    #
-    # print
-    # print "three clusters, min:"
-    # for cluster in generate_clusters(base_cluster, 3):
-    #     print get_values(cluster)
-    #
-    # print
-    # print "three clusters, max:"
-    # base_cluster = bottom_up_cluster(inputs, max)
-    # for cluster in generate_clusters(base_cluster, 3):
-    #     print get_values(cluster)
+
+    print
+    print "three clusters, min:"
+    for cluster in generate_clusters(base_cluster, 3):
+        print get_values(cluster)
+
+    print
+    print "three clusters, max:"
+    base_cluster = bottom_up_cluster(inputs, max)
+    for cluster in generate_clusters(base_cluster, 3):
+        print get_values(cluster)
